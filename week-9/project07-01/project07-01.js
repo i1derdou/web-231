bee5e8409faacb863450b0430b4e048cda44cb21
David Clemens <dclemens@outlook.com>
2024-05-19 22:30:08 -0500

    Updated files and assets

---
+++ week-9/project07-01/project07-01.js
@@ -0,0 +1,37 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 7
+      Project 07-01
+
+      Project to validate a form used for setting up a new account
+      Author: David Clemens
+      Date:   2024-05-19
+
+      Filename: project07-01.js
+*/
+
+let signupForm = document.getElementById("signup");
+
+signupForm.addEventListener("submit", function(e) {
+   let pwd = document.getElementById("pwd").value;
+   let feedback = document.getElementById("feedback");
+   e.preventDefault();
+
+   let regex1 = /[A-Z]/;
+   let regex2 = /\d/;
+   let regex3 = /[!\$#%]/;
+
+if(pwd.length < 8) {
+  feedback.textContent = " Your password must be at least 8 characters";
+  } else if (regex1.test(pwd) === false) {
+      feedback.textContent = "Your password must include an upper case letter";
+    } else if (regex2.test(pwd) === false) {
+      feedback.textContent = "Your password must include a number";
+     } else if (regex3.test(pwd) === false) {
+      feedback.textContent = "Your password must include one of the following '!$#%'";
+    }
+    else {
+        signupForm.submit();
+    }
+}
+);
